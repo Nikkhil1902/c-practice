4c63f196fc667be2a9099ab4e7a625de84fcdb55
Nikkhil1902 <107607589+Nikkhil1902@users.noreply.github.com>
2023-01-15 21:23:31 +0530

    Create choclates if else.cpp

---
+++ choclates if else.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+ int main(){
+
+ int choclates;
+ cin>>choclates;
+ if(choclates<=10){
+    cout<<"nidhi"<<endl;
+}
+ else if(choclates<=20){
+    cout<<"nikk"<<endl;
+}
+ else if(choclates<=50){
+    cout<<"kids"<<endl;
+}
+ else if(choclates<=100){
+    cout<<"family"<<endl;
+}
+ else if(choclates<=200){
+    cout<<"neighour"<<endl;
+}
+ else{
+  cout<<"frosty"<<endl;
+}
+ return 0;
+}
